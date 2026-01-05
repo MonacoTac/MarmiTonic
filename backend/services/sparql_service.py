ee4193569d6109ce37b70f0d1847ea9b9564e879
louReinaK <lou.r--k@outlook.fr>
2026-01-05 09:31:03 +0100

    Refactor SparqlService to use SPARQLWrapper for executing queries

--- backend/services/sparql_service.py
+++ backend/services/sparql_service.py
@@ -1,9 +1,14 @@
 from ..utils.dbpedia_client import query_dbpedia
+from SPARQLWrapper import SPARQLWrapper, JSON
 
 class SparqlService:
     def __init__(self):
         pass
     
     def execute_query(self, query: str):
-        results = query_dbpedia(query)
+        sparql = SPARQLWrapper("https://dbpedia.org/sparql")
+        
+        sparql.setQuery(query)
+        sparql.setReturnFormat(JSON)
+        results = sparql.query().convert()
         return results
